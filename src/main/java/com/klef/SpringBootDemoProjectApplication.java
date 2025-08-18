32685a9818da78428da6cabea847447c24767987
SAI <2300033275@kluniversity.in>
2025-08-18 20:08:06 +0530

    spring boot project v1.0

---
+++ src/main/java/com/klef/SpringBootDemoProjectApplication.java
@@ -0,0 +1,20 @@
+package com.klef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBootDemoProjectApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootDemoProjectApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoProjectApplication.class, args);
+        System.out.println("Hello DevOps : )");
+    }
+}
